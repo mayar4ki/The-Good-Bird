60ff50e8ed9ba12758471143fa81ac9096d20da9
Mayar <mayar.alidib@alternativa.dev>
2024-12-01 06:52:43 +0400

    Update PlayerScript.cs
    kill the player if he get out of borders

--- Assets/Modules/Player/PlayerScript.cs
+++ Assets/Modules/Player/PlayerScript.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BirdScript : MonoBehaviour
@@ -6,13 +9,32 @@ public class BirdScript : MonoBehaviour
     public Rigidbody2D birdRigidbody2D;
     public float jumpStringth=12;
     public bool isAlive = true; 
+
     private GameLogic gameLogic;
+    private readonly Dictionary<string, int> yLifeZone = new Dictionary<string, int>
+    {
+     { "lowerBound", -19 },
+     { "upperBound", 12 }
+    };
+
+
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         gameLogic = GameObject.FindGameObjectWithTag(Tags.GameLogic).GetComponent<GameLogic>();
     }
+
+
+    private void killObject()
+    {
+        if (!isAlive) return;
+
+        gameLogic.showGameOver();
+        isAlive = false;
+    }
+
 
     // Update is called once per frame
     void Update()
@@ -21,11 +43,20 @@ public class BirdScript : MonoBehaviour
         {
             birdRigidbody2D.linearVelocity = Vector2.up * jumpStringth;
         }
+
+        float y = transform.position.y;
+
+        if (y <= yLifeZone["lowerBound"] || y >= yLifeZone["upperBound"])
+        {
+            killObject();
+        }
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        gameLogic.showGameOver();
-        isAlive = false;
+       killObject();
     }
+
+
 }
